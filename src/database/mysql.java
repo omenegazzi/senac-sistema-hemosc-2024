b8033844a7f9cea771c02f962fe9a69aa539a64b
Diego <omenegazzi@gmail.com>
2024-06-12 14:51:40 -0300

    Primeiro commit do projeto no repositório.

---
+++ src/database/mysql.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author diegomenegazzi
+ */
+public class mysql {
+    public static Connection conexao(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            return DriverManager.getConnection("jdbc:mysql://localhost/db_biblioteca_senac?" +
+                                   "user=diego&password=1234");
+        } catch (Exception ex) {
+            throw new RuntimeException("Erro ao conectar no banco de dados");
+        }
+    }
+}
