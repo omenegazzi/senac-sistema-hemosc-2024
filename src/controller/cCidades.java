7b4921b395ee9e65160ec6c4723e645a59ccaed3
Diego <omenegazzi@gmail.com>
2024-06-20 13:01:13 -0300

    Merge origin/#23#31#44 into develop
    # Conflicts:
    #	nbproject/project.properties
    #	src/controller/cDoadores.java
    #	src/database/mysql.java
    #	src/view/vPrincipal.form
    #	src/view/vPrincipal.java

--- src/controller/cCidades.java
+++ src/controller/cCidades.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package controller;
 
@@ -19,9 +18,8 @@ import model.mCidades;
 
 /**
  *
- * @author leandrison.ribeiro
+ * @author diegomenegazzi
  */
-
 public class cCidades {
 
     public List<mcidades> listar() {
